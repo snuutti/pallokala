eee4c6399f5758f6015f612fa86a4063dcfd6f5c
Nuutti <snuutti@protonmail.com>
2025-01-18 04:11:07 +0200

    Fixed list modal scrollbar sometimes flashing

--- components/modal/ListModal.tsx
+++ components/modal/ListModal.tsx
@@ -78,6 +78,7 @@ export default function ListModal(props: ListModalProps) {
                 )}
                 estimatedItemSize={39}
                 onContentSizeChange={onContentSizeChange}
+                showsVerticalScrollIndicator={listHeight > 1}
             />
         </View>
     );
