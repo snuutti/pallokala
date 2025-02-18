0c9d0bad6bebc9e104307473767fa431ce422724
Nuutti <snuutti@protonmail.com>
2025-02-18 18:37:20 +0200

    WIP Tasks

--- types/server.ts
+++ types/server.ts
@@ -1,6 +1,19 @@
-import { ServerStatus, ServerView, FileDesc } from "pufferpanel";
+import { ServerStatus, ServerView, FileDesc, ConditionalMetadataType } from "pufferpanel";
 
 export type ExtendedServerStatus = ServerStatus | "loading" | undefined;
 export type ExtendedServerView = ServerView & { online: ExtendedServerStatus };
 
 export type ExtendedFileDesc = FileDesc & { path: string };
+
+export type ServerTasks = {
+    tasks: Record<string, ServerTask>;
+};
+
+export type ServerTask = {
+    name: string;
+    cronSchedule: string;
+    description?: string;
+    operations?: ConditionalMetadataType[];
+};
+
+export type ExtendedServerTask = ServerTask & { id: string };
