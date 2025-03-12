14f6824f45736382ac1ca2a287a71437b97cb40a
Nuutti <snuutti@protonmail.com>
2025-03-12 15:57:22 +0200

    Design install, hooks and json screens

--- app/(modal)/template/[id]/hooks.tsx
+++ app/(modal)/template/[id]/hooks.tsx
@@ -1,3 +1,43 @@
+import { Text, StyleSheet } from "react-native";
+import { useTranslation } from "react-i18next";
+import ContentWrapper from "@/components/screen/ContentWrapper";
+import OperatorList from "@/components/templates/operator/OperatorList";
+import { useStyle } from "@/hooks/useStyle";
+
 export default function HooksScreen() {
-    return null;
+    const { t } = useTranslation();
+    const { style } = useStyle((colors) =>
+        StyleSheet.create({
+            header: {
+                color: colors.text,
+                fontSize: 16
+            },
+            description: {
+                color: colors.text,
+                marginBottom: 10
+            }
+        })
+    );
+
+    return (
+        <ContentWrapper>
+            <Text style={style.header}>{t("templates:PreRunHook")}</Text>
+            <Text style={style.description}>{t("templates:description.PreRunHook")}</Text>
+
+            <OperatorList
+                operations={[]}
+                setOperations={() => {}}
+                addLabel={t("templates:AddPreStep")}
+            />
+
+            <Text style={style.header}>{t("templates:PostRunHook")}</Text>
+            <Text style={style.description}>{t("templates:description.PostRunHook")}</Text>
+
+            <OperatorList
+                operations={[]}
+                setOperations={() => {}}
+                addLabel={t("templates:AddPostStep")}
+            />
+        </ContentWrapper>
+    );
 }
--- app/(modal)/template/[id]/install.tsx
+++ app/(modal)/template/[id]/install.tsx
@@ -1,3 +1,29 @@
+import { Text, StyleSheet } from "react-native";
+import { useTranslation } from "react-i18next";
+import ContentWrapper from "@/components/screen/ContentWrapper";
+import OperatorList from "@/components/templates/operator/OperatorList";
+import { useStyle } from "@/hooks/useStyle";
+
 export default function InstallScreen() {
-    return null;
+    const { t } = useTranslation();
+    const { style } = useStyle((colors) =>
+        StyleSheet.create({
+            description: {
+                color: colors.text,
+                marginBottom: 10
+            }
+        })
+    );
+
+    return (
+        <ContentWrapper>
+            <Text style={style.description}>{t("templates:description.Install")}</Text>
+
+            <OperatorList
+                operations={[]}
+                setOperations={() => {}}
+                addLabel={t("templates:AddInstallStep")}
+            />
+        </ContentWrapper>
+    );
 }
