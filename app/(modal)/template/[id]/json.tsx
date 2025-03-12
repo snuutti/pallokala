1fee4a8bd2046edf7b2f74449d918ccfff9b1510
Nuutti <snuutti@protonmail.com>
2025-03-12 15:57:22 +0200

    Design install, hooks and json screens

--- app/(modal)/template/[id]/json.tsx
+++ app/(modal)/template/[id]/json.tsx
@@ -1,3 +1,26 @@
+import { StyleSheet } from "react-native";
+import CodeEditor from "@/components/ui/CodeEditor";
+import { useTemplateEditor } from "@/context/TemplateEditorProvider";
+import { useStyle } from "@/hooks/useStyle";
+
 export default function JsonScreen() {
-    return null;
+    const { style } = useStyle(() =>
+        StyleSheet.create({
+            codeEditor: {
+                flex: 1
+            }
+        })
+    );
+    const { json } = useTemplateEditor();
+
+    console.log(json);
+
+    return (
+        <CodeEditor
+            style={style.codeEditor}
+            content={json || null}
+            fileName="template.json"
+            onContentChange={() => {}}
+        />
+    );
 }
