4627daf106eddb6451536103763db01d44d69989
Nuutti <snuutti@protonmail.com>
2025-06-20 23:17:36 +0300

    Implemented template deleting

--- components/templates/TemplateActions.tsx
+++ components/templates/TemplateActions.tsx
@@ -1,5 +1,10 @@
+import { router } from "expo-router";
 import { useTranslation } from "react-i18next";
 import Button from "@/components/ui/Button";
+import { useApiClient } from "@/context/ApiClientProvider";
+import { useTemplateEditor } from "@/context/TemplateEditorProvider";
+import { useModal } from "@/context/ModalProvider";
+import useToast from "@/hooks/useToast";
 
 type TemplateActionsProps = {
     isValid: boolean;
@@ -7,6 +12,35 @@ type TemplateActionsProps = {
 
 export default function TemplateActions(props: TemplateActionsProps) {
     const { t } = useTranslation();
+    const { apiClient } = useApiClient();
+    const { template } = useTemplateEditor();
+    const { createAlertModal } = useModal();
+    const { showSuccessAlert } = useToast();
+
+    const deleteAlert = () => {
+        createAlertModal(
+            t("templates:ConfirmDelete", { name: template!.display || template!.name }),
+            undefined,
+            [
+                {
+                    text: t("templates:Delete"),
+                    icon: "trash-can",
+                    style: "danger",
+                    onPress: deleteTemplate
+                },
+                {
+                    text: t("common:Cancel"),
+                    icon: "close"
+                }
+            ]
+        );
+    };
+
+    const deleteTemplate = async () => {
+        await apiClient!.template.delete(template!.name);
+        showSuccessAlert(t("templates:Deleted"));
+        router.dismissTo("/(app)/templates");
+    };
 
     return (
         <>
@@ -14,14 +48,15 @@ export default function TemplateActions(props: TemplateActionsProps) {
                 text={t("templates:Delete")}
                 icon="trash-can"
                 style="danger"
-                onPress={() => {}}
+                onPress={deleteAlert}
+                disabled={!template || template.repository !== 0}
             />
 
             <Button
                 text={t("templates:Save")}
                 icon="content-save"
                 onPress={() => {}}
-                disabled={!props.isValid}
+                disabled={!props.isValid || !template || template.repository !== 0}
             />
         </>
     );
