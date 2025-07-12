0fd51cb46cd42b4f436638adac12ceeeac7ad491
Nuutti <snuutti@protonmail.com>
2025-07-12 22:08:22 +0300

    Added variable editing validation

--- app/(modal)/editvariablegroup.tsx
+++ app/(modal)/editvariablegroup.tsx
@@ -1,19 +1,38 @@
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { router } from "expo-router";
 import { useTranslation } from "react-i18next";
 import LoadingScreen from "@/components/screen/LoadingScreen";
 import ContentWrapper from "@/components/screen/ContentWrapper";
-import TextInput from "@/components/ui/TextInput";
+import FormTextInput from "@/components/ui/form/FormTextInput";
 import Button from "@/components/ui/Button";
 import { useBoundStore } from "@/stores/useBoundStore";
+
+const schema = z.object({
+    display: z.string().trim().min(1, { message: "templates:errors.DisplayInvalid" }),
+    description: z.string(),
+    condition: z.string().optional()
+});
+
+type Schema = z.infer<typeof schema>;
+
+const defaultValues: Schema = {
+    display: "",
+    description: "",
+    condition: ""
+};
 
 export default function EditVariableGroup() {
     const { t } = useTranslation();
     const initialVariableGroupData = useBoundStore(state => state.initialVariableGroupData);
     const setReturnedVariableGroupData = useBoundStore(state => state.setReturnedVariableGroupData);
-    const [display, setDisplay] = useState("");
-    const [description, setDescription] = useState("");
-    const [condition, setCondition] = useState("");
+    const { control, handleSubmit, setValue, formState: { errors, isValid } } = useForm<Schema>({
+        defaultValues,
+        resolver: zodResolver(schema),
+        mode: "onBlur"
+    });
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -23,18 +42,18 @@ export default function EditVariableGroup() {
             return;
         }
 
-        setDisplay(initialVariableGroupData.display);
-        setDescription(initialVariableGroupData.description);
-        setCondition(initialVariableGroupData.if || "");
+        setValue("display", initialVariableGroupData.display, { shouldValidate: true });
+        setValue("description", initialVariableGroupData.description, { shouldValidate: true });
+        setValue("condition", initialVariableGroupData.if, { shouldValidate: true });
         setLoading(false);
     }, [initialVariableGroupData]);
 
-    const save = () => {
+    const save = (data: Schema) => {
         setReturnedVariableGroupData({
             ...initialVariableGroupData!,
-            display,
-            description,
-            if: condition
+            display: data.display,
+            description: data.description,
+            if: data.condition
         });
 
         router.back();
@@ -46,30 +65,33 @@ export default function EditVariableGroup() {
 
     return (
         <ContentWrapper>
-            <TextInput
-                value={display}
-                onChangeText={setDisplay}
+            <FormTextInput
+                control={control}
+                name="display"
                 placeholder={t("templates:Display")}
+                error={errors.display?.message}
             />
 
-            <TextInput
-                value={description}
-                onChangeText={setDescription}
+            <FormTextInput
+                control={control}
+                name="description"
                 placeholder={t("templates:variables.Description")}
+                error={errors.description?.message}
             />
 
-            <TextInput
-                value={condition}
-                onChangeText={setCondition}
+            <FormTextInput
+                control={control}
+                name="condition"
                 placeholder={t("common:Conditions")}
                 description={t("templates:variables.ConditionHint")}
+                error={errors.condition?.message}
             />
 
             <Button
                 text={t("common:Apply")}
                 icon="content-save"
-                onPress={save}
-                // TODO: add disabled state
+                onPress={handleSubmit(save)}
+                disabled={!isValid}
             />
         </ContentWrapper>
     );
