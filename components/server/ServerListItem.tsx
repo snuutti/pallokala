ec00fccdf52918dd2087595128f3a4006f08038b
Nuutti <snuutti@protonmail.com>
2026-02-11 22:15:44 +0200

    Server list item backgrounds, more server icons

--- components/server/ServerListItem.tsx
+++ components/server/ServerListItem.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { router } from "expo-router";
-import { Image } from "expo-image";
+import { Image, ImageBackground } from "expo-image";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useStyle } from "@/hooks/useStyle";
 import { ExtendedServerView } from "@/types/server";
@@ -15,11 +15,42 @@ const icons: Record<string, string> = {
     "eco": require("@/assets/images/icons/eco.png"),
     "factorio": require("@/assets/images/icons/factorio.png"),
     "gmod": require("@/assets/images/icons/gmod.png"),
+    "hytale": require("@/assets/images/icons/hytale.png"),
     "minecraft-bedrock": require("@/assets/images/icons/minecraft-bedrock.png"),
     "minecraft-java": require("@/assets/images/icons/minecraft-java.png"),
     "rust": require("@/assets/images/icons/rust.png"),
+    "satisfactory": require("@/assets/images/icons/satisfactory.png"),
+    "squad": require("@/assets/images/icons/squad.png"),
     "starbound": require("@/assets/images/icons/starbound.png"),
-    "terraria": require("@/assets/images/icons/terraria.png")
+    "terraria": require("@/assets/images/icons/terraria.png"),
+    "tf2": require("@/assets/images/icons/tf2.png"),
+    "valheim": require("@/assets/images/icons/valheim.png"),
+    "vintage-story": require("@/assets/images/icons/vintage-story.png"),
+    "zomboid": require("@/assets/images/icons/zomboid.png")
+};
+
+const heros: Record<string, string> = {
+    "7days2die": require("@/assets/images/heros/7days2die.jpg"),
+    "ark": require("@/assets/images/heros/ark.jpg"),
+    "arma3": require("@/assets/images/heros/arma3.jpg"),
+    "csgo": require("@/assets/images/heros/csgo.jpg"),
+    "css": require("@/assets/images/heros/css.jpg"),
+    "dontstarvetogether": require("@/assets/images/heros/dontstarvetogether.jpg"),
+    "eco": require("@/assets/images/heros/eco.jpg"),
+    "factorio": require("@/assets/images/heros/factorio.jpg"),
+    "gmod": require("@/assets/images/heros/gmod.jpg"),
+    "hytale": require("@/assets/images/heros/hytale.jpg"),
+    "minecraft-bedrock": require("@/assets/images/heros/minecraft-bedrock.jpg"),
+    "minecraft-java": require("@/assets/images/heros/minecraft-java.jpg"),
+    "rust": require("@/assets/images/heros/rust.jpg"),
+    "satisfactory": require("@/assets/images/heros/satisfactory.jpg"),
+    "squad": require("@/assets/images/heros/squad.jpg"),
+    "starbound": require("@/assets/images/heros/starbound.jpg"),
+    "terraria": require("@/assets/images/heros/terraria.jpg"),
+    "tf2": require("@/assets/images/heros/tf2.jpg"),
+    "valheim": require("@/assets/images/heros/valheim.jpg"),
+    "vintage-story": require("@/assets/images/heros/vintage-story.jpg"),
+    "zomboid": require("@/assets/images/heros/zomboid.jpg")
 };
 
 type ServerListItemProps = {
@@ -27,17 +58,52 @@ type ServerListItemProps = {
 };
 
 export default function ServerListItem(props: ServerListItemProps) {
-    const { style, colors } = useStyle((colors) =>
+    const { style } = useStyle((colors) =>
         StyleSheet.create({
             server: {
                 height: 70,
+                marginHorizontal: 10,
+                marginVertical: 5
+            },
+            background: {
+                height: "100%",
+                backgroundColor: colors.background,
+                borderRadius: 15,
                 padding: 15,
                 flexDirection: "row",
-                backgroundColor: colors.background,
-                marginHorizontal: 10,
-                marginVertical: 5,
-                borderRadius: 15
+                boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)"
             },
+            backgroundImage: {
+                borderRadius: 15,
+                filter: "opacity(0.15)"
+            }
+        })
+    );
+
+    const onPress = () => {
+        router.push(`/(app)/server/${props.server.id}`);
+    };
+
+    if (!props.server.icon || !heros[props.server.icon]) {
+        return (
+            <TouchableOpacity style={[style.background, style.server]} onPress={onPress}>
+                <ServerListItemContent server={props.server} />
+            </TouchableOpacity>
+        );
+    }
+
+    return (
+        <TouchableOpacity style={style.server} onPress={onPress}>
+            <ImageBackground source={heros[props.server.icon]} style={style.background} imageStyle={style.backgroundImage}>
+                <ServerListItemContent server={props.server} />
+            </ImageBackground>
+        </TouchableOpacity>
+    );
+}
+
+function ServerListItemContent(props: ServerListItemProps) {
+    const { style, colors } = useStyle((colors) =>
+        StyleSheet.create({
             infoView: {
                 flex: 1,
                 flexGrow: 1,
@@ -102,12 +168,8 @@ export default function ServerListItem(props: ServerListItemProps) {
         return colors.textDisabled;
     }
 
-    const onPress = () => {
-        router.push(`/(app)/server/${props.server.id}`);
-    };
-
     return (
-        <TouchableOpacity style={style.server} onPress={onPress}>
+        <>
             <View style={style.infoView}>
                 <Text style={style.name} numberOfLines={1}>{props.server.name}</Text>
                 <Text style={style.node} numberOfLines={1}>{getServerAddress()} @ {props.server.node?.name}</Text>
@@ -131,6 +193,6 @@ export default function ServerListItem(props: ServerListItemProps) {
                     />
                 )}
             </View>
-        </TouchableOpacity>
+        </>
     );
 }
